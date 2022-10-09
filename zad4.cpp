ee0edbee434841bb020cc0b9b651a993c8e4928b
Stoyan Nikolov <nikolovs2003@abv.bg>
2022-10-10 00:41:43 +0300

    Create zad4.cpp

---
+++ zad4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+    int n;
+    do
+    {
+        std::cout << "Въведи нечетно число: ";
+        std::cin >> n;
+    } while (n % 2 == 0);
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int k = n - i; k > 0; k--)
+        {
+            std::cout << " ";
+        }
+        for (int p = 1; p <= i; p++)
+        {
+            std::cout << "# ";
+        }
+        std::cout << std::endl;
+    }
+}
