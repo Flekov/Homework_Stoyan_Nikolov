fce34ba87f3998460e5aa53b0fb1a9040c34f1d7
Stoyan Nikolov <nikolovs2003@abv.bg>
2022-10-09 23:24:51 +0300

    Create zad1.cpp

---
+++ zad1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    int n, k, i = 0, x = 1;
+    std::cin >> n >> k;
+
+    while (x <= n)
+    {
+        ++i;
+        x = pow(k, i);
+    }
+    std::cout << i - 1 << std::endl;
+}
