c0580cea9792d821804e51871deb2cb856d3c148
Stoyan Nikolov <nikolovs2003@abv.bg>
2022-10-09 23:53:51 +0300

    zad2

---
+++ zad2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+    int a, b, counter = 0;
+    std::cin >> a >> b;
+
+    for (int i = a; i <= b; i++)
+    {
+        int sum = 0, multi = 1, k = i;
+        while (k > 0)
+        {
+            sum += k % 10;
+            multi *= k % 10;
+            k = k / 10;
+        }
+        if (sum == multi)
+            ++counter;
+    }
+
+    std::cout << counter << std::endl;
+}
